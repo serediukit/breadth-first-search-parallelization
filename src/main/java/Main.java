ef3b9baf0e62b8b0573f22e5d548be5c204196a4
Valentyn Serediuk <serediukit@gmail.com>
2023-05-28 01:21:41 +0300

    update files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("MENU");
         System.out.println("----");
-        System.out.println("1. Search");
+        System.out.println("1. Linear search");
         System.out.println("2. Parallel search");
         System.out.println("3. Search path to Vertex");
         System.out.println("----");
@@ -24,15 +24,20 @@ public class Main {
         int start = sc.nextInt();
 
         Result result;
+        long startTime = 0, endTime = 0;
 
         switch (choice) {
             case 1 -> {
+                startTime = System.currentTimeMillis();
                 result = bfs.search(--start);
+                endTime = System.currentTimeMillis();
                 result.printDistance();
             }
             case 2 -> {
                 try {
+                    startTime = System.currentTimeMillis();
                     result = bfs.parallelSearch(--start, 5);
+                    endTime = System.currentTimeMillis();
                     result.printDistance();
                 } catch (InterruptedException | ExecutionException e) {
                     e.printStackTrace();
@@ -41,12 +46,21 @@ public class Main {
             case 3 -> {
                 System.out.print("End node: ");
                 int end = sc.nextInt();
+                startTime = System.currentTimeMillis();
                 result = bfs.searchToVertex(--start, --end);
+                endTime = System.currentTimeMillis();
                 result.printPath();
             }
             default -> {
                 System.out.println("Incorrect input");
             }
         }
+
+        long wastedTime = endTime - startTime;
+        long seconds = wastedTime / 1000;
+        long milliseconds = wastedTime % 1000;
+        String formattedTime = String.format("%02ds:%03dms", seconds, milliseconds);
+
+        System.out.println("Wasted time: " + formattedTime);
     }
 }
